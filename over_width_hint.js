b78962712b1eb21d2c80e356ed19d028976c6120
koolfret@163.com <koolfret@163.com>
2017-03-15 13:50:02 +0800

    初始化

---
+++ over_width_hint.js
@@ -0,0 +1,33 @@
+function setOverWidthHint(targ,height){
+	$(document).ready(function(){
+
+		$("body").prepend("<div style=\"position:relative;display:none;\" id=\"ow_hint_parent\"><span id=\"ow_hint_info\" style=\"position: absolute;background-color: #f0c53a;padding:5px;border-radius: 2px;\"></span></div>");
+		
+		
+		var targs=$(targ);
+		
+		for(var i=0;i<targs.size();i++){
+			var obj=targs.get(i);
+			var jObj=$(obj);
+			var txt=jObj.html();
+			
+			if (obj.offsetWidth < obj.scrollWidth) {		
+				jObj.mouseenter(function(curObj){
+					
+					var x=$(curObj.target).offset().top;
+					var y=$(curObj.target).offset().left;
+					var owHintInfo=$("#ow_hint_info");
+					owHintInfo.css("top",x+height);
+					owHintInfo.css("left",y);
+					owHintInfo.html($(curObj.target).html());
+					$("#ow_hint_parent").show();
+				});
+				jObj.mouseleave(function(curObj){
+					$("#ow_hint_parent").hide();
+				});				
+		    }
+			
+	}
+	});
+}
+	
